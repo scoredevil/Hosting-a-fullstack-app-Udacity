fe342412eca74125870c7953d70c6dd90922bdfe
scoredevil <71385271+scoredevil@users.noreply.github.com>
2022-10-09 02:42:28 +0200

    Update deploy.sh

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,1 @@
-aws s3 cp --recursive public-read ./www s3://udagram-93/
-aws s3 cp --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagram-93/
+aws s3 cp --recursive --acl ./www s3://udagram-93/
